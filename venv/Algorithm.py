5f8a38affd3edaf9dd5193b8a255b1fb5fa14590
rittmang <ritomgupta99@gmail.com>
2019-09-02 22:14:07 +0530

    Correct next_hop
    Signed-off-by: rittmang <ritomgupta99@gmail.com>

--- venv/Algorithm.py
+++ venv/Algorithm.py
@@ -6,7 +6,7 @@ class Algorithm(object):
     @staticmethod
     def shareRT(vertexList):
 
-        print("Routing Tables being transmitted 1 by 1:")
+        print("---------------------\n\nRouting Tables being transmitted 1 by 1:")
         #UPDATE_HAPPENED = False
 
         while True:
@@ -15,9 +15,11 @@ class Algorithm(object):
                 print("-----------------------------------------------")
                 for sender in receiver.neighbours:
                     current_cost = 0
+                    next_hop = '#'
                     for lol in receiver.routingTable:
                         if lol[0] == sender.name:
                             current_cost = lol[1]
+                            next_hop = lol[2]
                     print(sender.name," sends its DV to ",receiver.name," , and is at distance ",current_cost," from it.")
 
                     for i in receiver.routingTable:
@@ -28,7 +30,7 @@ class Algorithm(object):
                                 print(L1, "\t", " and ",end='')
                                 print(L2, " ",end='')
                                 L1[1] = L2[1]+current_cost
-                                L1[2] = sender.name
+                                L1[2] = next_hop#changed from sender.name
                                 receiver.routingTable.remove((i[0],i[1],i[2]))
                                 i = tuple(L1)
                                 receiver.routingTable.append(i)
@@ -38,7 +40,7 @@ class Algorithm(object):
                                 continue
                         #i = tuple(L1)
                 if UPDATE_HAPPENED == True:
-                    print("-----------------UPDATION------------------------")
+                    print("\n\nUPDATED ",end='')
                     print(receiver.name, "'s Routing Table:")
                     receiver.routingTable.sort()
                     for j in receiver.routingTable:
